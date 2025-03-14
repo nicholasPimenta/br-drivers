fccdc2ce619b38c5afc731ecb0635b9389950c3e
Nicholas Pimenta <nicholas2ilva@gmail.com>
2025-03-14 17:13:56 -0300

    Change from App.tsx to Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,7 @@ import { Home } from "./Pages/Home";
 import { Bortoleto } from "./Pages/Bortoleto";
 import { Camara } from "./Pages/Camara";
 
-export function App() {
+export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./App";
+import { Router } from "./Router";
 import "./styles/global.css"; // Arquivo global de estilos
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Router />
     </BrowserRouter>
   </React.StrictMode>
 );
