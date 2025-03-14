bd7252febf70da1cf1f5484701188700d64ccc01
Nicholas Pimenta <nicholas2ilva@gmail.com>
2025-03-14 18:41:02 -0300

    Fixed Imports again

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
-import "./styles/Global.css"; // Arquivo global de estilos
+import "./Styles/Global.css"; // Arquivo global de estilos
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
